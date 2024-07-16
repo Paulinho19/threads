763cc852476ac80bfd2c57abdcd57f3ce0ff8b93
Paulo <paulohsantosrocha@gmail.com>
2024-07-16 20:03:37 -0300

    feat: add rightsidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ClerkProvider>
           <Topbar />
-          <main>
+          <main className="flex flex-row">
             <LeftSideBar />
             <section className="main-container">
               <div className="w-full max-w-4xl">{children}</div>
